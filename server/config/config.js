747426f47710774a449e53362254459a4bdccbff
Enzo <enzoforbe@gmail.com>
2020-03-10 21:04:41 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,11 @@
+//entorno:
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+
+
 process.env.PORT = process.env.PORT || 3000;
+
+
+//Omongodb+srv://strider1:<password>@cluster0-lvnch.mongodb.net/test
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express()
 
 const bodyParser = require('body-parser');
@@ -11,43 +13,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuarios.js'));
 
-app.get('/Usuario', function(req, res) {
-    res.json('get Usuario');
-});
+mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
 
-app.post('/Usuario', function(req, res) {
+        if (err) throw err;
 
-    let body = req.body;
+        console.log('Base de datos ONLINE');
 
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-
-        res.json({
-            persona: body
-        });
-
-    }
-});
-
-app.put('/Usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-    res.json({
-        id
     });
-});
-
-app.delete('/Usuario', function(req, res) {
-    res.json('delete Usuario');
-});
-
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando al puerto: ', 3000);
